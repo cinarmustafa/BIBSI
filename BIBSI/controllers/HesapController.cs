b0e2b0a2726c3a068fee3efca42add8687b4677a
turankaptan <turan1988119@gmail.com>
2017-05-02 19:11:08 +0300

    Controller sınıfları düzenlendi. HesapController oluşturuldu.

---
+++ BIBSI/controllers/HesapController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIBSI.Controllers
+{
+    public class HesapController : Controller
+    {
+        // GET: Hesap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult YeniUyelik()
+        {
+            return View();
+        }
+
+        public ActionResult UyeGirisi()
+        {
+            return View();
+        }
+    }
+}
