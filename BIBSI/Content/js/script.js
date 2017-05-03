fc616e48e7c531a7270577c8165cf865846e747a
turankaptan <turan1988119@gmail.com>
2017-05-03 17:03:26 +0300

    Arayuzde iyilestirmeler yapildi. Ek sayfalar olusturuldu

--- BIBSI/Content/js/script.js
+++ BIBSI/Content/js/script.js
@@ -1,1 +1,6 @@
-﻿
+﻿function menuSec(id) {
+    $("#menu form ul li").each(function () {
+        $(this).removeClass("current_page_item");
+    });
+    $("#menu form ul li:nth-child(" + id + ")").addClass("current_page_item");
+}
